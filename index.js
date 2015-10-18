a7afb00f59f9fa11fb68581aa18b7e047076a536
Matt Howell <misterinterrupt@gmail.com>
2015-10-17 18:33:38 -0700

    converted to my-local-netmask

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 
 var n = require('os').networkInterfaces()
 
-var myIp = module.exports = function () {
-  var ip = []
+var myNetmask = module.exports = function () {
   for(var k in n) {
     var inter = n[k]
     for(var j in inter)
       if(inter[j].family === 'IPv4' && !inter[j].internal)
-        return inter[j].address
+        return inter[j].netmask
   }
 }
 
 if(!module.parent)
-  return console.log(myIp())
+  return console.log(myNetmask())
